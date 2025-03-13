17e32845e144f17c2701d56431ac85a54af3d8bb
vonedward <me@edward-vonschondorf.dev>
2025-03-13 13:48:58 -0700

    Add all sections to home page, get blog section loader working, refactor content more, and other minor changes

--- src/content/home/home-content.ts
+++ src/content/home/home-content.ts
@@ -61,3 +61,50 @@ export const interest = {
     text: "PC Building",
   },
 };
+
+export const techList = [
+  {
+    name: "JavaScript",
+    icon: "",
+  },
+  {
+    name: "TypeScript",
+    icon: "",
+  },
+  {
+    name: "Python",
+    icon: "",
+  },
+  {
+    name: "React",
+    icon: "",
+  },
+  {
+    name: "MySQL",
+    icon: "",
+  },
+  {
+    name: "MongoDB",
+    icon: "",
+  },
+  {
+    name: "Figma",
+    icon: "",
+  },
+  {
+    name: "Next",
+    icon: "",
+  },
+  {
+    name: "Astro",
+    icon: "",
+  },
+  {
+    name: "Node",
+    icon: "",
+  },
+  {
+    name: "Tailwind",
+    icon: "",
+  },
+];
