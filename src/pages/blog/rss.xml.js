dc309045153ce524293377c94846bca3376168d8
vonedward <me@edward-vonschondorf.dev>
2025-04-03 22:04:51 -0700

    Add a bunch of SEO related info to the head and other minor unrelated changes

--- src/pages/blog/rss.xml.js
+++ src/pages/blog/rss.xml.js
@@ -1,5 +1,7 @@
 import rss from "@astrojs/rss";
+
 import { getCollection } from "astro:content";
+
 import { SITE_TITLE, SITE_DESCRIPTION } from "../global-consts";
 
 export async function GET(context) {
